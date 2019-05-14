bb00638ebd431463afeff9b2ec453d97fc9106c4
Augusto Rucle <augustorucle@gmail.com>
2019-05-14 03:09:42 -0500

    Se agregaron randoms y diferencias a las flechas junto con arrows

--- tree.py
+++ tree.py
@@ -5,6 +5,7 @@
 from tkinter import *
 from tkinter import ttk
 from lecturaCodifoC import FunctionDetector
+import random
 
 #########################################################
 # CONSTANT
@@ -43,7 +44,7 @@ def text (canvas, x, y, text, font = ("bold", 10)):
         canvas.create_text (x, y, text = text, font = font)
 
 def line(canvas, startX, startY, endX, endY):
-        canvas.create_line(startX, startY, endX, endY)
+        canvas.create_line(startX, startY, endX, endY, arrow=LAST)
 
 def save_circle (id, startX, startY, endX, endY):
         dictionary_cicles[id] = { 
@@ -57,26 +58,27 @@ def draw_graph(canvas, functions_total):
         print(functions_total)
         draw_node_father(canvas, functions_total)
         draw_conection(canvas, functions_total)
-        draw_node_father(canvas, functions_total)
+        # draw_node_father(canvas, functions_total)
 
 def draw_conection(canvas, functions_total):
         for function_name, values in functions_total:
                 for child in values:
                         line( canvas,
-                              (dictionary_cicles[function_name]['startX'] + dictionary_cicles[function_name]['endX']) / 2,
-                              (dictionary_cicles[function_name]['startY'] + dictionary_cicles[function_name]['endY']) / 2,
-                              (dictionary_cicles[child]['startX'] + dictionary_cicles[child]['endX']) / 2,
-                              (dictionary_cicles[child]['startY'] + dictionary_cicles[child]['endY']) / 2)
+                              (dictionary_cicles[function_name]['startX'] + dictionary_cicles[function_name]['endX']) / 2 - 30,
+                              (dictionary_cicles[function_name]['startY'] + dictionary_cicles[function_name]['endY']) / 2 ,
+                              (dictionary_cicles[child]['startX'] + dictionary_cicles[child]['endX']) / 2 + 30,
+                              (dictionary_cicles[child]['startY'] + dictionary_cicles[child]['endY']) / 2 + 20)
 
 
 def draw_node_father(canvas, functions_total):
         later_id = 0
         for key, value in enumerate(functions_total):
+                number_random = random.randint(0, 240)
                 if(key == 0):
                         id = circle(canvas, startX= init_position + INCREMENT_X, 
-                                        startY=init_position + INCREMENT_Y, 
+                                        startY=init_position + INCREMENT_Y  + number_random, 
                                         endX=init_position + DIFFERENCE + INCREMENT_X, 
-                                        endY=init_position +DIFFERENCE + INCREMENT_Y, 
+                                        endY=init_position +DIFFERENCE + INCREMENT_Y  + number_random, 
                                         name=value[0])
 
                         text(canvas, 
@@ -87,9 +89,9 @@ def draw_node_father(canvas, functions_total):
                         later_id = id
                 elif((key % 2) != 0):
                         id = circle(canvas, startX=dictionary_cicles[later_id]['startX'] + INCREMENT_X * MULTI, 
-                                        startY=INCREMENT_Y,
+                                        startY=INCREMENT_Y  + number_random,
                                         endX=dictionary_cicles[later_id]['endX'] + INCREMENT_X * MULTI,
-                                        endY=INCREMENT_Y + DIFFERENCE,
+                                        endY=INCREMENT_Y + DIFFERENCE  + number_random,
                                         name=value[0])
 
                         text(canvas,
